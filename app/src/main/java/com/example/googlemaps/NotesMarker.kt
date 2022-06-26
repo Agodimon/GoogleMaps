cadd3a2da54cb3103469d7e9a24ff99dc0b5a33c
dmitryagoshkov <agodimon@mail.ru>
2022-06-26 10:10:29 +0500

    добавил второй экран для просмотра заметок с маркерами

---
+++ app/src/main/java/com/example/googlemaps/NotesMarker.kt
@@ -0,0 +1,10 @@
+package com.example.googlemaps
+
+data class NotesMarker (
+        val id: Int,
+        val nameMarker: String,
+        val address: String,
+        val latitude: String,
+        val longitude: String,
+
+)
---
+++ app/src/main/java/com/example/googlemaps/ui/OnCategoryClickListener.kt
@@ -0,0 +1,8 @@
+package com.example.googlemaps.ui
+
+import com.example.googlemaps.NotesMarker
+
+
+interface OnCategoryClickListener {
+    fun onClick(category: NotesMarker)
+}
