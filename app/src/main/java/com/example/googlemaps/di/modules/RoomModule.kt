18c06ef5423b48270e7f4abebc6064a8c7473525
dmitryagoshkov <agodimon@mail.ru>
2022-06-26 17:35:39 +0500

    выполнил сохранение в room

--- app/src/main/java/com/example/googlemaps/di/modules/RoomModule.kt
+++ app/src/main/java/com/example/googlemaps/di/modules/RoomModule.kt
@@ -1,6 +1,6 @@
 package com.example.googlemaps.di.modules
 
-import com.example.googlemaps.aplication.App
+import com.example.googlemaps.application.App
 import com.example.googlemaps.data.dao.NotesMarkersDao
 import com.example.googlemaps.data.entities.NotesMarkerEntity
 
